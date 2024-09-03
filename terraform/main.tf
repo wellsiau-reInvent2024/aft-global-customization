a5452fc5ad4482520384c4e5f0028a7c7f7c39c5
wellsiau-aws <wellsiau@amazon.com>
2024-09-02 21:07:59 -0700

    fix providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,6 @@ resource "aws_s3_account_public_access_block" "block_all_public" {
 }
 
 module "bedrock_guardrail_us_east_1" {
-  providers = {
-    aws   = aws
-    awscc = awscc
-  }
   source = "./modules/bedrock"
 }
 
