652c047ad7d7a7ca07837cf67550cd38e5b33991
wellsiau-aws <wellsiau@amazon.com>
2024-09-02 20:36:22 -0700

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_account_public_access_block" "block_all_public" {
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "awscc_bedrock_guardrail" "general" {
