b553b031c768b755914e3173bcdddb64acfd1320
wellsiau-aws <wellsiau@amazon.com>
2024-09-03 21:42:03 -0700

    config rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,19 @@ resource "aws_s3_account_public_access_block" "block_all_public" {
   restrict_public_buckets = true
 }
 
+resource "awscc_config_config_rule" "s3_versioning" {
+  config_rule_name = "S3_bucket_versioning_enabled"
+  description      = "Rule to validate if S3 Bucket versioning is enabled"
+  evaluation_modes = [{
+    mode = "DETECTIVE"
+  }]
+
+  source = {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
+  }
+}
+
 module "bedrock_guardrail_us_east_1" {
   source = "./modules/bedrock"
 }
