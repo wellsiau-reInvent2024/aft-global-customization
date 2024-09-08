bf6986f569b76d1483f7a88e7e18efe1e7f3f0d6
wellsiau-aws <wellsiau@amazon.com>
2024-09-07 21:51:31 -0700

    reduce file size

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,4 +1,4 @@
-module "default_account_config" {
-  source                     = "./modules/config"
-  conformance_pack_file_name = "Operational-Best-Practices-for-AI-and-ML"
+module "config_conformance_pack" {
+  source                = "./modules/config"
+  conformance_pack_name = "Operational-Best-Practices-for-AI-and-ML"
 }
