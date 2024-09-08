59e81f7afc78f8f6e42088b88e2fe8883fe315cd
wellsiau-aws <wellsiau@amazon.com>
2024-09-07 22:27:06 -0700

    pre-api

--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
+which jinja2
 jinja2 --format=yaml $DEFAULT_PATH/terraform/config.jinja $DEFAULT_PATH/terraform/deployment_manifest.yaml > $DEFAULT_PATH/terraform/config.tf
